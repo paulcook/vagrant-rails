b9336fc7d017cd365eddf768bab0b87285eb8225
Paul Cook <cook.424@osu.edu>
2014-09-19 14:03:39 -0400

    added provisioning scripts for pg, rvm

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison nodejs
